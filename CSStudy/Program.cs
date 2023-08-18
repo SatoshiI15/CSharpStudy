e174bfa8fb093f398fd08a2d845e7c4f3941663e
Satoshi Iwai <siwai@lenovo.com>
2023-08-18 11:35:32 +0900

    Initial commit

---
+++ CSStudy/Program.cs
@@ -0,0 +1,15 @@
+﻿var names = new List<string> { "Satoshi", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+
