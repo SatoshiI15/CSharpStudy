4c9a730ed3a9f69e01a640a00552bce3dd72237a
Satoshi Iwai <siwai@lenovo.com>
2023-09-12 11:05:52 +0900

    End of Array

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int[] nums1 = new int[] { 1, 2, 3 };
+            int[,] arr = { { 1, 2, 3 }, { 3, 4, 5, }, { 6, 7, 8, } };
 
-            int[] nums2 = nums1.ToArray();
+            int lengthAll = arr.Length;
+            int length1 = arr.GetLength(0);
+            int length2 = arr.GetLength(1);
+
+            Console.WriteLine(lengthAll);
+            Console.WriteLine(length1);
+            Console.WriteLine(length2);
         }
     }
 }
