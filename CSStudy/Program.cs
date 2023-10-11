3b10baad8c958c71830decc4587f9624108f1739
Satoshi Iwai <siwai@lenovo.com>
2023-10-11 11:34:29 +0900

    LINQ walkthrough

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -1,22 +1,39 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Security.Cryptography;
+using System.Xml.Schema;
 
 namespace CSStudy
 {
-    class Program
+    public class Fundamentals
     {
+        public class Student
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int ID { get; set; }
+            public List<int> Scores;
+        }
+
+        // Create a data source by using a collection initializer.
+        static List<Student> students = new List<Student>
+        {
+            new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+            new Student {First="Claire", Last="O'Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+            new Student {First="Sven", Last="Mortensen", ID=113, Scores= new List<int> {88, 94, 65, 91}},
+            new Student {First="Cesar", Last="Garcia", ID=114, Scores= new List<int> {97, 89, 85, 82}},
+            new Student {First="Debra", Last="Garcia", ID=115, Scores= new List<int> {35, 72, 91, 70}},
+            new Student {First="Fadi", Last="Fakhouri", ID=116, Scores= new List<int> {99, 86, 90, 94}},
+            new Student {First="Hanying", Last="Feng", ID=117, Scores= new List<int> {93, 92, 80, 87}},
+            new Student {First="Hugo", Last="Garcia", ID=118, Scores= new List<int> {92, 90, 83, 78}},
+            new Student {First="Lance", Last="Tucker", ID=119, Scores= new List<int> {68, 79, 88, 92}},
+            new Student {First="Terry", Last="Adams", ID=120, Scores= new List<int> {99, 82, 81, 79}},
+            new Student {First="Eugene", Last="Zabokritski", ID=121, Scores= new List<int> {96, 85, 91, 60}},
+            new Student {First="Michael", Last="Tucker", ID=122, Scores= new List<int> {94, 92, 91, 91}},
+            new Student {First="Michael", Last="Jordan", ID=123, Scores=new List<int> {79,99,69,87}}
+        };
+
         static void Main(string[] args)
         {
-            string strEmpty = string.Empty;
-            string strNull = null;
 
-            if(string.IsNullOrEmpty(strEmpty))
-            {
-                Console.WriteLine("strEmpty is an empty string");
-            }
-            if(string.IsNullOrEmpty(strNull))
-            {
-                Console.WriteLine("strNull is a null string");
-            }
         }
     }
 }
